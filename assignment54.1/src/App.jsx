4aa0afb20ad03f753d92d8e97df0673da3c65f35
Mohit Kumar <mohitkumarsolanki25112005@gmail.com>
2025-08-02 09:30:17 +0530

    Initial commit: Add assignment54.1 files

---
+++ assignment54.1/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Card from './components/Card'
+import Main from './components/Main'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
