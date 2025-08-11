6bb67d164c9e66489bb8246d875e58b1310bb4db
Mohit Kumar <mohitkumarsolanki25112005@gmail.com>
2025-08-11 10:23:54 +0530

    assignment57.1 in this the console log give an error of prodectlistand card

--- assignment57.1/src/components/ProductList.jsx
+++ assignment57.1/src/components/ProductList.jsx
@@ -4,9 +4,10 @@ import Card from './Card'
 const ProductList = ({products}) => {
   return (
     <div className='flex flex-wrap gap-4'>
-        {products.map(function(items){
+        {products.map(function(items, index){
             return (
                 <Card
+                    key={index}
                     img={items.img}
                     type={items.type}
                     title={items.title}
